79774a6ded2184a4de37b2ce7c73a5006d17b2ad
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-11-04 09:26:40 +0100

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -6,9 +6,9 @@ namespace OpenLab2._7a
 {
     class Christmas
     {
-        public bool IsTimeForCookies(int year, int month, int day)
+        public bool IsTimeForCookies(int month, int day)
         {
-            if (year == 2019 && month == 12 && day == 24)
+            if (month == 12 && day == 24)
             {
                 return true;
             }
